6d40539d5759429e567e81e4958c714b330b2795
TanishaDas <metanisha19@gmail.com>
2024-07-18 20:23:57 +0530

    watermelon

--- Watermelon.java
+++ Watermelon.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class Solution{
+public class Watermelon{
     public static void main(String args[]){
         Scanner scan=new Scanner(System.in);
         int n= scan.nextInt();
